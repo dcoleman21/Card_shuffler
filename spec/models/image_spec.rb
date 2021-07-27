33cef3c0cbdde18a5d360ffd4fada50c45b854d4
Dani Coleman <danicoleman00@gmail.com>
2021-07-27 16:42:01 -0600

    Add relationships and validations

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  describe 'Relationships' do
+    it { should belong_to :card }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :source }
+  end
+end
