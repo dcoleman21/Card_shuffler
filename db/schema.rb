085d9a4b1b256dfb7c1de28909f66264f9a9f4cf
Dani Coleman <danicoleman00@gmail.com>
2021-07-27 09:54:50 -0600

    Create decks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_154059) do
+ActiveRecord::Schema.define(version: 2021_07_27_155145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2021_07_27_154059) do
     t.string "source"
     t.string "quote"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "decks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
