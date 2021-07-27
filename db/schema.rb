0deaaba928e8d42dde74bfb4137baabcb234bb1d
Dani Coleman <danicoleman00@gmail.com>
2021-07-27 10:00:09 -0600

    Create joins table deck_cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_155145) do
+ActiveRecord::Schema.define(version: 2021_07_27_155727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,20 @@ ActiveRecord::Schema.define(version: 2021_07_27_155145) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "deck_cards", force: :cascade do |t|
+    t.bigint "deck_id", null: false
+    t.bigint "card_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["card_id"], name: "index_deck_cards_on_card_id"
+    t.index ["deck_id"], name: "index_deck_cards_on_deck_id"
+  end
+
   create_table "decks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "deck_cards", "cards"
+  add_foreign_key "deck_cards", "decks"
 end
