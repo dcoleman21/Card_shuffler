f87719439efe4bc34175c1bd8f9dc475db481855
Dani Coleman <danicoleman00@gmail.com>
2021-08-02 14:30:34 -0600

    Update cards controller to call Card model method

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,6 @@
 class CardsController < ApplicationController
   def index
-    CardFacade.create_affirmation_cards
-    @cards = Card.all
+    all_cards = CardFacade.create_affirmation_cards
+    @featured_card = all_cards.get_random_card
   end
 end
